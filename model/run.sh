09559e85fa0c00d21104646ca971501aa3b3741c
Dreamer <47875143+YuanchenBei@users.noreply.github.com>
2023-02-22 18:34:57 +0800

    Update run.sh

--- model/run.sh
+++ model/run.sh
@@ -1,2 +1,2 @@
-python main.py --model_name cgi --dataset_name ml-10m --learning_rate 0.005 --weight_decay 0.0001
-python main.py --model_name cgi --dataset_name electronics --learning_rate 0.005 --weight_decay 0.00005
+python main.py --model_name nrcgi --dataset_name ml-10m --learning_rate 0.005 --weight_decay 0.0004
+python main.py --model_name nrcgi --dataset_name electronics --learning_rate 0.005 --weight_decay 0.00005
